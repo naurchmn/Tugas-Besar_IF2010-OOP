661e77beece759b2b20c70efa3d25d7991279e88
carlenalt <carlenasadel@gmail.com>
2025-05-29 04:37:13 +0700

    ShippingBin dan Gold

--- Gold.java
+++ Gold.java
@@ -1,2 +1,45 @@
+package com.spakborhills.model.currency;
+
 public class Gold {
+    private int amount;
+
+    public Gold() {
+        this.amount = 0;
+    }
+
+    public Gold(int initialAmount) {
+        this.amount = Math.max(0, initialAmount);
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = Math.max(0, amount);
+    }
+
+    public void add(int value) {
+        if (value > 0) {
+            amount += value;
+        }
+    }
+
+    public boolean subtract(int value) {
+        if (value <= amount) {
+            amount -= value;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean canAfford(int value) {
+        return amount >= value;
+    }
+
+    @Override
+    public String toString() {
+        return amount + "g";
+    }
 }
+
